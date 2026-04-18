4c12fbedf64cad54c39a0f5b757f3e88e43c8bb6
maimoona1234-lab <fhina2510@gmail.com>
2026-04-18 16:09:24 +0500

    push

---
+++ Assignment 4/app.js
@@ -0,0 +1,31 @@
+// question 1
+// let name = ("Maimoona");
+// let age = ("16");
+// let education = ("Matric");
+
+// question 2
+
+// LEGALL
+// var myName;
+// var _age;
+// var $price;
+// var user1;
+// var firstName;
+
+// ILLEGAL
+// var 1name;      // cannot start with number
+// var my-name;    // hyphen not allowed
+// var var;        // reserved keyword
+// var user name;  // space not allowed
+// var @price;     // special character not allowed
+
+
+// question 3
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("Variable names can only contain letters, numbers, $, and _. For example $my_1stVariable <br><br>");
+
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name <br><br>");
+
+// document.write("Variable names are case sensitive <br><br>");
+
+// document.write("Variable names should not be JS reserved keywords");
