c8903b9b90a59f98882d5a5f6d1983cdf4613bc9
maimoona1234-lab <fhina2510@gmail.com>
2026-04-18 15:26:36 +0500

    push

---
+++ Assignment 3/app.js
@@ -0,0 +1,31 @@
+// question  1
+// let age = 15; 
+// alert("I am " + age + " years old");
+
+// question 2
+// let visits = localStorage.getItem("visitCount");
+
+// if (visits === null) {
+//     visits = 1;
+// } else {
+//     visits = Number(visits) + 1;
+// }
+
+// localStorage.setItem("visitCount", visits);
+
+// alert("You have visited this site " + visits + " times");
+
+
+// question 3
+// let birthYear = 1990;  
+
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared variable is " + typeof birthYear);
+
+
+// question 4
+// let visitorName = "John Doe";
+// let productTitle = "T-shirt";
+// let quantity = 5;
+
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
