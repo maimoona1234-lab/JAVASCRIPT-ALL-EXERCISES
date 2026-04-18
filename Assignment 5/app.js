7fab9c274f322a444df49426c9a407831c77fb96
maimoona1234-lab <fhina2510@gmail.com>
2026-04-18 16:31:39 +0500

    push

---
+++ Assignment 5/app.js
@@ -0,0 +1,86 @@
+// question 1
+// let num1 = 3;
+// let num2 = 5;
+
+// let sum = num1 + num2;
+
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+// question 2
+// let num1 = 3;
+// let num2 = 5;
+// Subtraction
+// let sub = num1 - num2;
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + sub + "<br>");
+// Multiplication
+// let mul = num1 * num2;
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + mul + "<br>");
+// // Division
+// let div = num1 / num2;
+// document.write("Division of " + num1 + " and " + num2 + " is " + div + "<br>");
+// Modulus
+// let mod = num1 % num2;
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + mod);
+
+// question 3
+// let num;
+// b.
+// document.write("Value after variable declaration is: " + num + "<br>");
+// c.
+// num = 5;
+// d.
+// document.write("Initial value: " + num + "<br>");
+// e
+// num++;
+// F
+// document.write("Value after increment is: " + num + "<br>");
+// g.
+// num = num + 7;
+// h..
+// document.write("Value after addition is: " + num + "<br>");
+// i. 
+// num--;
+// j.
+// document.write("Value after decrement is: " + num + "<br>");
+// k.
+// let remainder = num % 3;
+// l.
+// document.write("The remainder is: " + remainder);
+
+// question 4
+// let ticketPrice = 600;
+// let numberOfTickets = 5;
+
+// let totalCost = ticketPrice * numberOfTickets;
+
+// document.write("Total cost to buy " + numberOfTickets + " tickets to a movie is " + totalCost + " PKR");
+
+// question 5
+// let num = 4;
+
+// for (let i = 1; i <= 10; i++) {
+//     document.write(num + " x " + i + " = " + (num * i) + "<br>");
+// }
+
+
+// questio 6
+  // a. 
+// let celsius = 25;
+
+// b.
+// let fahrenheit = (celsius * 9/5) + 32;
+// document.write(celsius + "°C is " + fahrenheit + "°F<br><br>");
+
+
+// c.
+// let fTemp = 77;
+
+// d.
+// let cTemp = (fTemp - 32) * 5/9;
+// document.write(fTemp + "°F is " + cTemp + "°C");
+
+
+// question 7
+
+
+
